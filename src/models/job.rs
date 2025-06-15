2cb1f493afa55355407e2f7660a060e8459c8f9f
Alan Matthew <alan.matthew.yk@gmail.com>
2025-06-15 17:36:10 +0800

    init

---
+++ src/models/job.rs
@@ -0,0 +1,14 @@
+pub enum JobStage {
+    Applied,
+    Assessment,
+    WaitingForInterview,
+    WaitingAfterInterview,
+    Rejected,
+    Offered,
+    Withdrew,
+}
+
+pub struct Job {
+    title: String,
+
+}
